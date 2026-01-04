46b6aa5cadf59c2b2821d509bd76d35061c99fd4
Aleksandr7023 <grudnick@mail.ru>
2026-01-04 15:53:39 +0400

    test bump v1.24 → v1.25 + обновления (04.01.26 15:53:39)

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
+// main.jsx
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import TestMailScreen from './screens/TestMailScreen.jsx';
-import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
