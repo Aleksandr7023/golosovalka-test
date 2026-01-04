90cb567533c1e166e724a79558556c0d51e6041f
Aleksandr7023 <grudnick@mail.ru>
2026-01-04 15:50:21 +0400

    test bump v1.23 → v1.24 + обновления (04.01.26 15:50:21)

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-// App.jsx (в golosovalka_test)
-import React from 'react';
-import TestMailScreen from './screens/TestMailScreen.jsx';
-
-export default function App() {
-  return <TestMailScreen />;
-}
