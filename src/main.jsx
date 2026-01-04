24d6b5d7a5bd3ccabcbb74e2dc11d610076c4e5d
Aleksandr7023 <grudnick@mail.ru>
2026-01-04 15:48:51 +0400

    test bump v1.22 → v1.23 + обновления (04.01.26 15:48:51)

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-// main.jsx
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App.jsx';
+import TestMailScreen from './screens/TestMailScreen.jsx';
 import './index.css';
+
 createRoot(document.getElementById('root')).render(
-<React.StrictMode>
-<BrowserRouter>
-<App />
-</BrowserRouter>
-</React.StrictMode>
+  <React.StrictMode>
+    <TestMailScreen />
+  </React.StrictMode>
 );
