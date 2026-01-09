b5ab242315c1d034a6e6e12fdafd9abac612e98e
Aleksandr7023 <grudnick@mail.ru>
2026-01-09 16:41:36 +0400

    test bump v1.123 → v1.124 + обновления (09.01.26 16:41:36)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 // App.jsx
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, ScrollRestoration } from 'react-router-dom';
 import MainScreen from './screens/MainScreen.jsx';
 
 export default function App() {
@@ -9,6 +8,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<MainScreen />} />
       </Routes>
+      <ScrollRestoration />
     </>
   );
 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-﻿export const APP_VERSION = 'v1.123';
+﻿export const APP_VERSION = 'v1.124';
 export const APP_NAME = 'Голосовалка';
