78fd1e2b91b967916984482f64a7defc7e94b7f7
Aleksandr7023 <grudnick@mail.ru>
2026-01-09 15:34:05 +0400

    test bump v1.107 → v1.108 + обновления (09.01.26 15:34:05)

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 // main.jsx
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom/client'; // ← правильный импорт
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
